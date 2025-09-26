108aba010dea96ceed1ea572ea97261264d62301
Luka Piplica <piplicaluka64@gmail.com>
2025-09-25 20:30:01 -0400

    Added game state global store

---
+++ src/game_state.lua
@@ -0,0 +1,29 @@
+local game_state = {}
+
+-- Bishops
+game_state.P1_BISHOP_1 = "P1_BISHOP_1"
+game_state.P1_BISHOP_2 = "P1_BISHOP_2"
+game_state.P2_BISHOP_1 = "P2_BISHOP_1"
+game_state.P2_BISHOP_2 = "P2_BISHOP_2"
+
+-- Kings
+game_state.P1_KING = "P1_KING"
+game_state.P2_KING = "P2_KING"
+
+-- Knights
+game_state.P1_KNIGHT_1 = "P1_KNIGHT_1"
+game_state.P1_KNIGHT_2 = "P1_KNIGHT_2"
+game_state.P2_KNIGHT_1 = "P2_KNIGHT_1"
+game_state.P2_KNIGHT_2 = "P2_KNIGHT_2"
+
+game_state.P1_PAWN_1 = "P1_PAWN_1"
+game_state.P1_PAWN_2 = "P1_PAWN_2"
+game_state.P1_PAWN_3 = "P1_PAWN_3"
+game_state.P1_PAWN_4 = "P1_PAWN_4"
+game_state.P1_PAWN_5 = "P1_PAWN_5"
+game_state.P1_PAWN_6 = "P1_PAWN_6"
+game_state.P1_PAWN_7 = "P1_PAWN_7"
+game_state.P1_PAWN_8 = "P1_PAWN_8"
+
+
+return game_state
--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,9 @@
+-- holds the images and positions of the pieces
+pieces = {}
+
 function love.load()
     love.window.setTitle("Chess Engine")
     love.window.setMode(800, 800)
-end
-
-function love.draw()
-    local tileWidth = love.graphics.getWidth() / 8
-    local tileHeight = love.graphics.getHeight() / 8
-
-    print("[DEBUG] Current working directory:", love.filesystem.getWorkingDirectory())
 
     local bishop = love.graphics.newImage("assets/bishop.png")
     local king = love.graphics.newImage("assets/king.png")
@@ -16,13 +12,23 @@ function love.draw()
     local queen = love.graphics.newImage("assets/queen.png")
     local rook = love.graphics.newImage("assets/rook.png")
 
+    -- figure out inital positions for each piece
+end
+
+function love.draw()
+    local tileWidth = love.graphics.getWidth() / 8
+    local tileHeight = love.graphics.getHeight() / 8
+
+    local game_state = require("game_state")
+    print(game_state.P1_BISHOP_1) -- Example usage of game_state
+
     -- Draw the board 8x8
     for i = 0, 7 do
         for j = 0, 7 do
             if (i + j) % 2 == 0 then
-                love.graphics.setColor(131, 89, 54)
+                love.graphics.setColor(love.math.colorFromBytes(131, 89, 54))
             else
-                love.graphics.setColor(191, 153, 90)
+                love.graphics.setColor(love.math.colorFromBytes(191, 153, 90))
             end
             love.graphics.rectangle("fill", i * tileWidth, j * tileHeight, tileWidth, tileHeight)
         end
